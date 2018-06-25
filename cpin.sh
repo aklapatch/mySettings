f4700334c80e0146140bfbe0ce3c1fb450fbb4c7
me <sniper96a@gmail.com>
2018-06-24 21:01:55 -0400

    automatic upload

--- cpin.sh
+++ cpin.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 echo "copying ~/$1 to $1"
 install ~/$1 $1
--- upload.sh
+++ upload.sh
@@ -1,15 +1,5 @@
-#/bin/sh
+#!/bin/sh
 
-
-files=$(cat files.txt) 
-dir=$(dirname "$0")
-
-for a in $files
-do
-	
-	git add $a
-done
-
-git add *
+git add -A
 git commit -m "automatic upload"
 git push origin master
